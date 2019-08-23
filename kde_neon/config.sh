624a5fd627166f68f213fb020e73614a9ad280e8
Angel Lopez Manriquez <sigma271@outlook.com>
2019-08-22 22:27:12 -0500

    Add files via upload

--- kde_neon/config.sh
+++ kde_neon/config.sh
@@ -33,5 +33,7 @@ function install_anaconda() {
 function install_vim() {
 	git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 	sh ~/.vim_runtime/install_awesome_vimrc.sh
+    cp my_configs.vim ~/.vim_runtime/my_configs.vim
+
 }
 
